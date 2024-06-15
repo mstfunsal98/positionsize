c72ca2a0eeac94f2c49a15f34f473dae4ffd9aee
mstfunsal98 <mstfunsal98@outlook.com>
2024-06-15 18:44:55 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function calculate() {
+    const fundSize = document.getElementById('fundSize').value;
+    const fundLoss = document.getElementById('fundLoss').value;
+    const stopLoss = document.getElementById('stopLoss').value;
+
+    if (fundSize && fundLoss && stopLoss) {
+        const result = (fundSize * fundLoss) / stopLoss;
+        document.getElementById('result').textContent = `Result: ${result}`;
+    } else {
+        document.getElementById('result').textContent = 'Please enter all values.';
+    }
+}
+
+function toggleDropdown() {
+    const dropdownContent = document.getElementById('dropdownContent');
+    dropdownContent.style.display = dropdownContent.style.display === 'flex' ? 'none' : 'flex';
+}
+
+function calculateStopPrice() {
+    const entryPrice = document.getElementById('entryPrice').value;
+    const stopLossPercent = document.getElementById('stopLossPercent').value;
+
+    if (entryPrice && stopLossPercent) {
+        const stopPrice = entryPrice * (1 - stopLossPercent / 100);
+        document.getElementById('stopPriceResult').textContent = `Stop Price: ${stopPrice}`;
+    } else {
+        document.getElementById('stopPriceResult').textContent = 'Please enter all values.';
+    }
+}
